da49554b8f3d684144ab0b10aad0784d4c71b02e
oskr_ <oskr_@LAPTOP-AS15LFEE>
2023-02-27 21:36:00 -0600

    Number of islands solved

---
+++ MaximumSubArray/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumSubArray
+{
+    public class Solution
+    {
+        //Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+        //Constrains
+        //1 <= nums.Length <= 10^5
+        //-10^4 <= nums[i] <= 10^4
+
+        //-2, 1, -3, 4, -1, 2, 1, -5, 4  
+        //5, 4, -1, 7, 8
+        //-3, -2, 0, -1 
+
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = nums[0];
+            uint length = (uint)nums.Length;
+
+            int currentSum = nums[0] < 0 ? 0 : nums[0] ;
+
+            for(uint i = 1; i < length; i++)
+            {
+                currentSum += nums[i];
+
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+                if (currentSum < 0)
+                {
+                    currentSum = 0;
+                }              
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ Number of Islands/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberOfIslands
+{
+    public class Solution
+    {
+        //["1","1","0","0","0"],
+        //["1","1","0","0","0"],
+        //["0","0","1","0","0"],
+        //["0","0","0","1","1"]
+        public int NumIslands(char[][] grid)
+        {
+            //Search for land
+            int rows = grid.Length, cols = grid[0].Length, islands = 0;
+
+            for(int j,i = 0; i < rows; i++)
+            {
+                for(j = 0; j < cols; j++)
+                {
+                    if(grid[i][j] == '1')
+                    {
+                        islands++;
+                        SetIslandFound(grid, i, j, rows, cols);
+
+                    }
+                }
+            }
+
+            return islands;
+           
+        }
+
+        public void SetIslandFound(char[][]grid, int row, int col, int limitRow, int limitCol)
+        {
+            //Stop recursive call
+            if (row < 0 || col < 0 || row >= limitRow || col >= limitCol || grid[row][col] != '1')
+                return;
+
+            //Search by adjacents horizontally and vertically
+            grid[row][col] = '_'; //Set as counted
+
+            SetIslandFound(grid, row + 1, col, limitRow, limitCol);
+            SetIslandFound(grid, row - 1, col, limitRow, limitCol);
+            SetIslandFound(grid, row, col + 1, limitRow, limitCol);
+            SetIslandFound(grid, row, col - 1, limitRow, limitCol);
+
+        }
+    }
+}
